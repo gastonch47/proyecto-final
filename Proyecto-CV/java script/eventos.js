a1cb8ad7da5b09e4a88afb24b7dac9073888882e
gastonch47 <102353327+gastonch47@users.noreply.github.com>
2023-01-31 22:58:36 -0300

    Add files via upload

---
+++ Proyecto-CV/java script/eventos.js
@@ -0,0 +1,37 @@
+function enGris1() {
+Element =document.getElementById("pers");
+Element.src="img/personal2.png";
+}
+function enNegro1() {
+    Element =document.getElementById("pers");
+    Element.src="img/personal.png";
+}
+
+function enGris2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral2.png";
+}
+ function enNegro2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral.png";
+}
+
+function enGris3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades2.png";
+}
+function enNegro3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades.png";
+}
+
+function enGris4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios2.png";
+}
+function enNegro4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios.png";
+}
+
+
